c4619d7fdefa07a99c8c2dd0ef5918ae72ffdb10
Glenn Reilly <glenn.reilly@outware.com.au>
2020-03-11 17:04:50 +1100

    init commit

---
+++ src/main/kotlin/testApp/App.kt
@@ -0,0 +1,18 @@
+package testApp
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main(args: Array<String>) {
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("My Example", ContentType.Text.Html)
+            }
+        }
+    }.start(wait = true)
+}
