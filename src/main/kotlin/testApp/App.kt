130ab914beaf639f237351d7ddb7a7ed7e6cd037
Glenn Reilly <glenn.reilly@outware.com.au>
2020-04-15 02:30:09 +1000

    added Docker

--- src/main/kotlin/testApp/App.kt
+++ src/main/kotlin/testApp/App.kt
@@ -2,17 +2,36 @@ package testApp
 
 import io.ktor.application.*
 import io.ktor.http.*
+import io.ktor.request.host
+import io.ktor.request.port
+import io.ktor.request.uri
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import org.joda.time.DateTime
 
-fun main(args: Array<String>) {
+fun main() {
     embeddedServer(Netty, 8080) {
         routing {
             get("/") {
-                call.respondText("My Example", ContentType.Text.Html)
+                val uri = call.request.uri
+                val host = call.request.host()
+                val port = call.request.port()
+                call.respondText("Hello World from Docker: ${DateTime.now().toString("dd/MMM/yyyy : hh:mm:ss:SS")} \n" +
+                        ", host: $host:$port", ContentType.Text.Html)
+            }
+
+            get("/now"){
+                call.respondText("New", ContentType.Text.Html)
             }
         }
     }.start(wait = true)
 }
+
+//./gradlew build
+//docker build --tag ktorexample:1.0 .
+//docker run -m512M --cpus 1 -it -p 8080:8080 --rm ktorexample:1.0
+//docker run --network=host -m512M --cpus 1 -it -p 8080:8080 --rm ktorexample:1.0
+//docker container ls
+//docker stop [containerName]
